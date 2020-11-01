eb65c1102dc14749831d32d55890c1150190057b
Raghavi02bala <raghavibalasubramania@gmail.com>
2020-11-01 12:06:57 +0530

    done

--- src/index.js
+++ src/index.js
@@ -5,16 +5,25 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Auth from './components/auth';
+import { API } from './api-services';
 
 export const TokenContext = createContext(null);
 
 function Router() {
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [token, setToken] = useState('');
+
+  const loginClicked = () => {
+    API.loginUser({username,password})
+        .then(resp => setToken(resp.token))
+        .then(error  => console.log(error))
+}
 
   return (
     <React.StrictMode>    
-      <TokenContext.Provider value={{token, setToken}}>      
+      <TokenContext.Provider value={{token, setToken, loginClicked, username, setUsername, password, setPassword}}>      
         <BrowserRouter>
           <Route exact path="/" component={Auth} />
           <Route exact path="/movies" component={App} />
